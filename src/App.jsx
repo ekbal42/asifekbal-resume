e08ba46ec0f8e3bc59e730adea24438c28731dc2
ekbal42 <asifekbaldev@gmail.com>
2024-07-16 19:39:20 +0600

    layout updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,96 +1,55 @@
+import { Experiences, PersonalProjects, Skills } from "./data";
+
 export default function App() {
   return (
-    <div className="bg-gray-100 md:py-12 h-full">
-      <div className="container mx-auto max-w-4xl border-[1px] bg-white">
-        <div className="border-b-[1px] p-4 flex flex-wrap gap-2 justify-between items-center">
-          <div className="">
-            <h1 className="text-2xl font-bold ">Asif Ekbal</h1>
-            <h4>Front End Developer</h4>
-          </div>
-          <div className="">
-            <div className="flex justify-start items-center gap-1">
-              <p className="font-medium">Porfolio :</p>
-              <a
-                href="https://asifekbal.vercel.app/"
-                className="hover:underline text-gray-600 "
-              >
-                asifekbal.vercel.app
-              </a>
-            </div>
-            <div className="flex justify-start items-center gap-1">
-              <p className="font-medium">Mail :</p>
-              <a
-                href="mailto:asifekbaldev@gmail.com"
-                className="hover:underline text-gray-600"
-              >
-                asifekbaldev@gmail.com
-              </a>
+    <div className="bg-gray-100 md:py-12 h-full ">
+      <div
+        className="container mx-auto max-w-4xl border-[1px] bg-white
+        px-4 md:p-14 lg:p-20
+        "
+      >
+        <div className="border-b-[1px] py-4 flex flex-wrap gap-2 justify-between items-center">
+          <div className="flex items-end gap-2">
+            <img src="/asif.jpg" className="w-16 h-16" alt="Asif Ekbal" />
+            <div>
+              <h1 className="text-2xl font-bold uppercase">Asif Ekbal</h1>
+              <h4>Front End Developer</h4>
             </div>
           </div>
         </div>
         <div className="">
-          <div className="border-b-[1px] p-4">
+          <div className="border-b-[1px] py-4">
             <SectionTitle title={"Technical Skills"} />
             <div>
-              <SkillCard
-                title="Languages"
-                skills="JavaScript (ES6+), TypeScript"
-              />
-              <SkillCard
-                title="Frameworks"
-                skills="Next.js, React.js, Express.js"
-              />
-              <SkillCard
-                title="UI Frameworks"
-                skills="Bootstrap 5, Tailwind CSS, Chakra UI"
-              />
-              <SkillCard
-                title="State Management"
-                skills="Redux Toolkit, Zustand"
-              />
-              <SkillCard title="Data Fetching" skills="React Query, Axios" />
-              <SkillCard title="Design Tools" skills="Figma" />
-              <SkillCard title="Database" skills="Prisma, MongoDB" />
-              <SkillCard title="Deployment Platforms" skills="Vercel" />
-              <SkillCard
-                title="Web Development Tools"
-                skills="VS Code, Postman, Chrome DevTools, GitHub"
-              />
-              <SkillCard
-                title="Other Skills"
-                skills="API Integration, REST APIs, CRUD Operations, User Authentication"
-              />
+              {Skills?.map((skill) => (
+                <SkillCard title={skill.title} skills={skill.skills} />
+              ))}
             </div>
           </div>
-          <div className="border-b-[1px] p-4">
+          <div className="border-b-[1px] py-4">
             <SectionTitle title={"Experiences"} />
             <div>
-              <ExperienceCard
-                title="Tuition Terminal"
-                duration="(April 2023 - May 2024)"
-                link="https://dev-tuitionterminal.vercel.app"
-                description="During my tenure at Tuition Terminal, I led the React development efforts for tuitionterminal.com.bd. Utilizing ReactJS and Vite for streamlined development, I focused on ensuring the website's responsiveness using Bootstrap for UI design. API integration was handled seamlessly with Axios, while Redux Toolkit facilitated robust state management for the application. The project was deployed on Vercel for scalable and reliable hosting."
-              />
-              <ExperienceCard
-                title="Porao"
-                duration="(June 2024 - Feb 2025)"
-                link="https://poraome.vercel.app"
-                description="At Porao, I served as a front-end developer, building the project entirely from scratch using React. I employed React Query for optimized data fetching and caching, and ensured a responsive design across mobile and larger displays with TailwindCSS. For global state management alongside React Query, Zustand was utilized. The project was developed in TypeScript for robust type checking and scalability."
-              />
+              {Experiences?.map((project) => (
+                <ExperienceCard
+                  title={project.title}
+                  duration={project.duration}
+                  link={project.link}
+                  description={project.description}
+                />
+              ))}
             </div>
           </div>
-          <div className=" p-4">
+          <div className="py-4">
             <SectionTitle title={"Personal Projects"} />
             <div>
-              <ExperienceCard
-                title="Napnux"
-                duration="(June 2022 - Dec 2023)"
-                link="https://github.com/Ekbal41/napnux"
-                description="Napnux is a lightweight and fast web framework for
-            Node.js, designed with low overhead similar to Express.js. It
-            offers the same APIs and is production-ready. For more details,
-            refer to the Napnux documentation."
-              />
+              {PersonalProjects?.map((project) => (
+                <ExperienceCard
+                  title={project.title}
+                  duration={project.duration}
+                  link={project.link}
+                  description={project.description}
+                />
+              ))}
             </div>
           </div>
         </div>
@@ -116,18 +75,10 @@ const ExperienceCard = ({ title, link, duration, description }) => {
   return (
     <div className="mb-6">
       <div>
-        <a
-          href={link}
-          target="_blank"
-          className="hover:underline font-semibold"
-        >
+        <a href={link} target="_blank" className="underline font-semibold">
           {title}
         </a>
-        <a
-          href={link}
-          target="_blank"
-          className="ml-2 text-gray-600 hover:underline"
-        >
+        <a href={link} target="_blank" className="ml-2 text-gray-600 underline">
           {duration}
         </a>
       </div>
---
+++ src/data.js
@@ -0,0 +1,82 @@
+export const Skills = [
+  {
+    title: "Languages",
+    skills: "JavaScript (ES6+), TypeScript",
+  },
+  {
+    title: "Frameworks",
+    skills: "Next.js, React.js, Express.js",
+  },
+  {
+    title: "UI Frameworks",
+    skills: "Bootstrap 5, Tailwind CSS, Chakra UI",
+  },
+  {
+    title: "State Management",
+    skills: "Redux Toolkit, Zustand",
+  },
+  {
+    title: "Data Fetching",
+    skills: "React Query, Axios",
+  },
+  {
+    title: "Design Tools",
+    skills: "Figma",
+  },
+  {
+    title: "Database",
+    skills: "Prisma, MongoDB",
+  },
+  {
+    title: "Deployment Platforms",
+    skills: "Vercel",
+  },
+  {
+    title: "Web Development Tools",
+    skills: "VS Code, Postman, Chrome DevTools, GitHub",
+  },
+  {
+    title: "Other Skills",
+    skills: "API Integration, REST APIs, CRUD Operations, User Authentication",
+  },
+];
+
+export const Experiences = [
+  {
+    title: "Tuition Terminal",
+    duration: "(April 2023 - May 2024)",
+    link: "https://dev-tuitionterminal.vercel.app",
+    description: `During my tenure at Tuition Terminal, 
+    I led the React development efforts for tuitionterminal.com.bd. 
+    Utilizing ReactJS and Vite for streamlined development, 
+    I focused on ensuring the website's responsiveness using Bootstrap 
+    for UI design. API integration was handled seamlessly with Axios, 
+    while Redux Toolkit facilitated robust state management for the application. 
+    The project was deployed on Vercel for scalable and reliable hosting.`,
+  },
+  {
+    title: "Porao",
+    duration: "(June 2024 - Feb 2025)",
+    link: "https://poraome.vercel.app",
+    description: `At Porao, I served as a front-end developer, 
+    building the project entirely from scratch using React. 
+    I employed React Query for optimized data fetching and 
+    caching, and ensured a responsive design across mobile 
+    and larger displays with TailwindCSS. For global state 
+    management alongside React Query, Zustand was utilized. 
+    The project was developed in TypeScript for robust type 
+    checking and scalability.`,
+  },
+];
+
+export const PersonalProjects = [
+  {
+    title: "Napnux",
+    duration: "(June 2022 - Dec 2023)",
+    link: "https://github.com/Ekbal41/napnux",
+    description: `Napnux is a lightweight and fast web framework for
+    Node.js, designed with low overhead similar to Express.js. It
+    offers the same APIs and is production-ready. For more details,
+    refer to the Napnux documentation.`,
+  },
+];
